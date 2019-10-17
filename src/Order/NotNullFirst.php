c09da97a3f2364812f6a9079b8dd2beefec5fbdf
codisart <louis.celier@gmail.com>
2019-10-18 01:03:26 +0200

    Add a custom expression to ordering not null first

---
+++ src/Order/NotNullFirst.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Storal\Order;
+
+use Storal\Column;
+use Zend\Db\Sql\Expression;
+
+class NotNullFirst extends Expression
+{
+    public function __construct(Column $column)
+    {
+        $expression = 'ISNULL(%s) ASC';
+        parent::__construct(
+            sprintf($expression, $column->getExpression())
+        );
+    }
+}
---
+++ tests/Order/NotNullFirstTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Storal\Order;
+
+use Storal\TableIdentifier;
+use PHPUnit\Framework\TestCase;
+
+class NotNullFirstTest extends TestCase
+{
+    public function testInstance()
+    {
+        $groupTable = new TableIdentifier('group');
+        $column = $groupTable->column('owner_id');
+
+        $expression = new NotNullFirst($column);
+        self::assertSame(
+            'ISNULL("group".owner_id) ASC',
+            $expression->getExpression()
+        );
+    }
+}
