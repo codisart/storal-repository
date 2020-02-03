matterstech/matters-repository

Add some models for column and tableidentifier

src/Column.php
@@ -1,0 +1,14 @@
+<?php
+
+namespace Matters;
+
+use Zend\Db\Sql\Expression;
+
+class Column extends Expression
+{
+    public function __construct(string $columnName,TableIdentifier $table)
+    {
+        $expression = sprintf('%s.%s', $table->getFullName(), $columnName);
+        parent::__construct($expression);
+    }
+}

src/TableIdentifier.php
@@ -1,0 +1,24 @@
+<?php
+/**
+ * @author Matters Studio (https://matters.tech)
+ */
+
+namespace Matters;
+
+use Zend\Db\Sql\TableIdentifier as ZendTableIdentifier;
+
+class TableIdentifier extends ZendTableIdentifier
+{
+    public function column(string $columnName)
+    {
+        return new Column($columnName, new self($this->table, $this->schema));
+    }
+
+    public function getFullName()
+    {
+        if (!$this->schema) {
+            return sprintf('"%s"', $this->table);
+        }
+        return sprintf('"%s"."%s"', $this->schema, $this->table);
+    }
+}

tests/TableIdentifierTest.php
@@ -1,0 +1,51 @@
+<?php
+
+
+namespace Matters;
+
+use PHPUnit\Framework\TestCase;
+use Zend\Db\Adapter\Platform\Postgresql;
+use Zend\Db\Sql\Select;
+
+class TableIdentifierTest extends TestCase
+{
+    public function testSelect_simpleTable()
+    {
+        $groupTable = new TableIdentifier('group');
+        $select = (new Select())->from($groupTable);
+
+        $ownerId = $groupTable->column('owner_id');
+
+        $select->columns(['owner_id' => $ownerId]);
+
+        self::assertSame(
+            'SELECT "group".owner_id AS "owner_id" FROM "group"',
+            $select->getSqlString()
+        );
+    }
+    public function testSelect_tableWithSchema()
+    {
+        $groupTable = new TableIdentifier('group', 'invoicing');
+        $select = (new Select())->from($groupTable);
+
+        $ownerId = $groupTable->column('owner_id');
+
+        $select->columns(['owner_id' => $ownerId]);
+
+        $select->where
+            ->equalTo($ownerId, 4);
+
+        $platform = new class extends Postgresql
+        {
+            public function quoteValue($value)
+            {
+                return $value;
+            }
+        };
+
+        self::assertSame(
+            'SELECT "invoicing"."group".owner_id AS "owner_id" FROM "invoicing"."group" WHERE "invoicing"."group".owner_id = 4',
+            $select->getSqlString($platform)
+        );
+    }
+}

codisart/storal-repository

src/Column.php
@@ -1,0 +1,14 @@
+<?php
+
+namespace Storal;
+
+use Zend\Db\Sql\Expression;
+
+class Column extends Expression
+{
+    public function __construct(string $columnName,TableIdentifier $table)
+    {
+        $expression = sprintf('%s.%s', $table->getFullName(), $columnName);
+        parent::__construct($expression);
+    }
+}

src/TableIdentifier.php
@@ -1,0 +1,21 @@
+<?php
+
+namespace Storal;
+
+use Zend\Db\Sql\TableIdentifier as ZendTableIdentifier;
+
+class TableIdentifier extends ZendTableIdentifier
+{
+    public function column(string $columnName)
+    {
+        return new Column($columnName, new self($this->table, $this->schema));
+    }
+
+    public function getFullName()
+    {
+        if (!$this->schema) {
+            return sprintf('"%s"', $this->table);
+        }
+        return sprintf('"%s"."%s"', $this->schema, $this->table);
+    }
+}

tests/TableIdentifierTest.php
@@ -1,0 +1,51 @@
+<?php
+
+
+namespace Storal;
+
+use PHPUnit\Framework\TestCase;
+use Zend\Db\Adapter\Platform\Postgresql;
+use Zend\Db\Sql\Select;
+
+class TableIdentifierTest extends TestCase
+{
+    public function testSelect_simpleTable()
+    {
+        $groupTable = new TableIdentifier('group');
+        $select = (new Select())->from($groupTable);
+
+        $ownerId = $groupTable->column('owner_id');
+
+        $select->columns(['owner_id' => $ownerId]);
+
+        self::assertSame(
+            'SELECT "group".owner_id AS "owner_id" FROM "group"',
+            $select->getSqlString()
+        );
+    }
+    public function testSelect_tableWithSchema()
+    {
+        $groupTable = new TableIdentifier('group', 'invoicing');
+        $select = (new Select())->from($groupTable);
+
+        $ownerId = $groupTable->column('owner_id');
+
+        $select->columns(['owner_id' => $ownerId]);
+
+        $select->where
+            ->equalTo($ownerId, 4);
+
+        $platform = new class extends Postgresql
+        {
+            public function quoteValue($value)
+            {
+                return $value;
+            }
+        };
+
+        self::assertSame(
+            'SELECT "invoicing"."group".owner_id AS "owner_id" FROM "invoicing"."group" WHERE "invoicing"."group".owner_id = 4',
+            $select->getSqlString($platform)
+        );
+    }
+}
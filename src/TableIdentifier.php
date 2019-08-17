98211ef52b7a7eb764987a2c9d6fa14a574ec9c6
codisart <louis.celier@gmail.com>
2019-08-17 16:17:06 -0700

    Add some models for column and tableidentifier

---
+++ src/TableIdentifier.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Storal;
+
+use Zend\Db\Sql\TableIdentifier as ZendTableIdentifier;
+
+class TableIdentifier extends ZendTableIdentifier
+{
+    public function column(string $columnName)
+    {
+        return new Column($columnName, new self($this->table, $this->schema));
+    }
+
+    public function getFullName()
+    {
+        if (!$this->schema) {
+            return sprintf('"%s"', $this->table);
+        }
+        return sprintf('"%s"."%s"', $this->schema, $this->table);
+    }
+}
