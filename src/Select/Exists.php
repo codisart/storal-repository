matterstech/matters-repository

Add exists select query type

src/Select/Exists.php
@@ -1,0 +1,36 @@
+<?php
+
+namespace Matters\Select;
+
+use Zend\Db\Sql\Expression;
+use Zend\Db\Sql\Select;
+use Zend\Db\Sql\TableIdentifier;
+
+class Exists extends Select
+{
+    const EXISTS_ALIAS = 'exists';
+
+    const EXISTS_EXPRESSION = "'exists'";
+
+    /**
+     * @param  null|string|array|TableIdentifier $table
+     */
+    public function __construct($table = null)
+    {
+        parent::__construct($table);
+        parent::columns([
+            self::EXISTS_ALIAS => new Expression(self::EXISTS_EXPRESSION)
+        ]);
+        parent::limit(1);
+    }
+
+    public function columns(array $columns, $prefixColumnsWithTable = true)
+    {
+        throw new \BadMethodCallException('The columns should not be changed');
+    }
+
+    public function limit($limit)
+    {
+        throw new \BadMethodCallException('The columns should not be changed');
+    }
+}

tests/Select/ExistsTest.php
@@ -1,0 +1,28 @@
+<?php
+
+namespace Matters\Select;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+use Zend\Db\Adapter\Platform\Postgresql;
+
+class ExistsTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    public function testInstance()
+    {
+        $select = (new Exists())->from('potatoes');
+        $select->where('potato_id = 42');
+
+        $platform = new class extends Postgresql
+        {
+            public function quoteValue($value)
+            {
+                return '"'. $value .'"';
+            }
+        };
+
+        self::assertSame('SELECT \'exists\' AS "exists" FROM "potatoes" WHERE potato_id = 42 LIMIT "1"', $select->getSqlString($platform));
+    }
+}

codisart/storal-repository

src/Select/Exists.php
@@ -1,0 +1,36 @@
+<?php
+
+namespace Storal\Select;
+
+use Zend\Db\Sql\Expression;
+use Zend\Db\Sql\Select;
+use Zend\Db\Sql\TableIdentifier;
+
+class Exists extends Select
+{
+    const EXISTS_ALIAS = 'exists';
+
+    const EXISTS_EXPRESSION = "'exists'";
+
+    /**
+     * @param  null|string|array|TableIdentifier $table
+     */
+    public function __construct($table = null)
+    {
+        parent::__construct($table);
+        parent::columns([
+            self::EXISTS_ALIAS => new Expression(self::EXISTS_EXPRESSION)
+        ]);
+        parent::limit(1);
+    }
+
+    public function columns(array $columns, $prefixColumnsWithTable = true)
+    {
+        throw new \BadMethodCallException('The columns should not be changed');
+    }
+
+    public function limit($limit)
+    {
+        throw new \BadMethodCallException('The columns should not be changed');
+    }
+}

tests/Select/ExistsTest.php
@@ -1,0 +1,28 @@
+<?php
+
+namespace Storal\Select;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+use Zend\Db\Adapter\Platform\Postgresql;
+
+class ExistsTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    public function testInstance()
+    {
+        $select = (new Exists())->from('potatoes');
+        $select->where('potato_id = 42');
+
+        $platform = new class extends Postgresql
+        {
+            public function quoteValue($value)
+            {
+                return '"'. $value .'"';
+            }
+        };
+
+        self::assertSame('SELECT \'exists\' AS "exists" FROM "potatoes" WHERE potato_id = 42 LIMIT "1"', $select->getSqlString($platform));
+    }
+}